e940062e7f5a6d7f320aacc9a35297491c8cea97
Ondrej Tucek <ondrej.tucek@gmail.com>
2016-08-13 16:09:40 +0200

    init commit

---
+++ {{cookiecutter.project_slug}}/README.rst
@@ -0,0 +1,19 @@
+===============================
+{{ cookiecutter.project_name }}
+===============================
+
+
+{{ cookiecutter.project_short_description}}
+
+
+Features
+--------
+
+* TODO
+
+
+
+
+
+
+
---
+++ {{cookiecutter.project_slug}}/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='{{ cookiecutter.project_slug }}',
+    version='{{ cookiecutter.version }}',
+    description="{{ cookiecutter.project_short_description }}",
+    long_description=readme + '\n\n' + history,
+    author="{{ cookiecutter.full_name }}",
+    author_email='{{ cookiecutter.email }}',
+    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}',
+    packages=[
+        '{{ cookiecutter.project_slug }}',
+    ],
+    package_dir={'{{ cookiecutter.project_slug }}':
+                 '{{ cookiecutter.project_slug }}'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    keywords='{{ cookiecutter.project_slug }}',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: MIT',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
